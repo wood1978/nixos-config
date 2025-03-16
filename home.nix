e835bb4e730d4e983d79889c2202934a3b7357e5
Wood Chan <ShihHsien.Chan@gmail.com>
2025-03-16 11:08:39 +0800

    * Add git alias.

--- home.nix
+++ home.nix
@@ -109,7 +109,16 @@
     userName = "Wood Chan";
     userEmail = "ShihHsien.Chan@gmail.com";
     aliases = {
-      st = "status";
+      s = "status";
+      co = "checkout";
+      cob = "checkout -b";
+      del = "branch -D";
+      br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
+      save = "!git add -A && git commit -m 'chore: commit save point";
+      undo = "reset HEAD~1 --mixed";
+      res = "!git reset --hard";
+      done = "!git push origin HEAD";
+      l = "!git log --pretty=format:\"%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]\" --abbrev-commit -30";
     };
   };
 
