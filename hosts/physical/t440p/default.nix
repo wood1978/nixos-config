df1a7cf44433faa0050b2786995c3ce2b4f2a248
Wood Chan <ShihHsien.Chan@gmail.com>
2026-01-06 17:50:57 +0800

    * Change T440P and T490 battary start charge level.

--- hosts/physical/t440p/default.nix
+++ hosts/physical/t440p/default.nix
@@ -38,7 +38,7 @@
 			CPU_MAX_PERF_ON_BAT = 20;
 
 			#Optional helps save long term battery health
-			START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+			START_CHARGE_THRESH_BAT0 = 60; # 40 and below it starts to charge
 			STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
 
 		};
