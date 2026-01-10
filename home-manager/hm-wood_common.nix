94e24131e0f44c7649f7b05634d503e9b6d8c0a3
WoodChan <ShihHsien.Chan@gmail.com>
2026-01-10 10:06:52 +0800

    * Add persepolis for wood.

--- home-manager/hm-wood_common.nix
+++ home-manager/hm-wood_common.nix
@@ -93,6 +93,7 @@
 		socat # replacement of openbsd-netcat
 		nmap # A utility for network discovery and security auditing
 		ipcalc	# it is a calculator for the IPv4/v6 addresses
+		persepolis # A download manager written in Python
 
 		# misc
 		file
