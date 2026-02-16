f4ca0503f47acb83858b63c471f3a2c16c9d6ff4
Wood Chan <ShihHsien.Chan@gmail.com>
2026-02-16 10:39:32 +0800

    * Add open port 24800 for Synergy.

--- hosts/physical/t14g6/default.nix
+++ hosts/physical/t14g6/default.nix
@@ -21,6 +21,8 @@
 	# networking.proxy.default = "http://user:password@proxy:port/";
 	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+	networking.firewall.allowedTCPPorts = [ 24800 ];
+
 	hardware.graphics = {
 		extraPackages = with pkgs; [
 			# your Open GL, Vulkan and VAAPI drivers
